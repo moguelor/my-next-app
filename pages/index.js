bf7c3d1ed57c8a7d64aca1ea80b0e6864f677e0e
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2023-06-30 14:25:55 -0600

    added more elements

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,14 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import Image from "next/imgae";
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
+        <Image alt="" src="/images/logo.svg" />
         <Link href="/docs/app"> THIS IS THE TEST LINK </Link>
+        <a className="footer"> </a>
       </main>
     </div>
   );
