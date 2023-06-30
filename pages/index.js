9de857f7abb8dd8350cc38778f097a89795a346a
Jose Obed Moguel Ruiz <jmoguelruiz@gmail.com>
2023-06-30 14:27:15 -0600

    fixed typo

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import styles from "../styles/Home.module.css";
 import Link from "next/link";
-import Image from "next/imgae";
+import Image from "next/image";
 
 export default function Home() {
   return (
